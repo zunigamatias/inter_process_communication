9703aed9283a0e24cb88bc429ca7c008d0a46707
zunigamatias <oliveirazmatias@gmail.com>
2025-08-25 01:32:23 -0300

    page styling decided, overall look created with two buttons representing two different processes.

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnProcessOne = document.getElementById("btn-process-one");
+const btnProcessTwo = document.getElementById("btn-process-two");
+
+const arrowOne = document.getElementById("arrowOne")
+const arrowTwo = document.getElementById("arrowTwo")
+
