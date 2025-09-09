078358306d9a412791186d454ff7b98de96a94a5
Zuniga Matias (CtP/ETS) <Matias.Zuniga@br.bosch.com>
2025-09-09 16:00:24 -0300

    server folder created, it acts like a bridge between front and backend

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import fs from "fs";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+app.use(bodyParser.json());
+
+// allow frontend (localhost:5500) to talk to backend (localhost:3000)
+app.use(cors({
+  origin: "http://localhost:5500"
+}));
+
+const fifoReq = "/tmp/fifo_req";
+const fifoRes = "/tmp/fifo_res";
+
+// send request to backend
+app.post("/send", (req, res) => {
+  const request = JSON.stringify(req.body);
+
+  // write request to FIFO
+  fs.writeFileSync(fifoReq, request + "\n");
+
+  // read response from backend
+  const response = fs.readFileSync(fifoRes, "utf-8");
+  res.json(JSON.parse(response));
+});
+
+app.listen(3000, () => {
+  console.log("Bridge running at http://localhost:3000");
+});
